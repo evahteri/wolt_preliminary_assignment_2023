dd6973ea1455f6fdb72edbbe24b49d4e057b5af3
Eero Vahteri <eero.vahteri@helsinki.fi>
2023-01-24 12:30:09 +0200

    created tests

--- src/tests/fee_calculator_test.py
+++ src/tests/fee_calculator_test.py
@@ -18,3 +18,31 @@ class TestFeeCalculator(unittest.TestCase):
         correct_fee = 710
         calculated_fee = self.test_fee_calculator.calculate_fee()
         self.assertEqual(correct_fee, calculated_fee)
+
+    def test_fee_calculator_cart_value_over_10000(self):
+        correct_fee = 0
+        test_response_object = ResponseObject(
+            cart_value=11000,
+            delivery_distance=2235,
+            number_of_items=4,
+            time="2021-10-12T13:00:00Z"
+        )
+        test_fee_calculator = FeeCalculator(response_object=
+        test_response_object
+        )
+        calculated_fee = test_fee_calculator.calculate_fee()
+        self.assertEqual(correct_fee, calculated_fee)
+    
+    def test_fee_calculator_cart_value_under_1000(self):
+        correct_fee = 310
+        test_response_object = ResponseObject(
+            cart_value=790,
+            delivery_distance=200,
+            number_of_items=4,
+            time="2021-10-12T13:00:00Z"
+        )
+        test_fee_calculator = FeeCalculator(response_object=
+        test_response_object
+        )
+        calculated_fee = test_fee_calculator.calculate_fee()
+        self.assertEqual(correct_fee, calculated_fee)
